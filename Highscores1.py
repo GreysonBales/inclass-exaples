72935c3c6a2b6fb55de2e1ac32e90c4b5ba5ff5e
Greyson Bales <greyson.bales@tooeleschools.org>
2019-09-05 13:46:39 -0600

    Finished highscore assignment

---
+++ Highscores1.py
@@ -0,0 +1,50 @@
+# Bales, Greyson
+# 9/5/19
+# highscore list
+
+
+#creating a highscore list
+#names for highscores
+
+name1   = "Rachel "
+name2   = "Greyson"
+name3   = "Rachle "
+name4   = "Grayson"
+name5   = "Raychl "
+name6   = "Gracin "
+name7   = "Raechle"
+name8   = "Graeson"
+name9   = "Rachil "
+name10  = "Grecin "
+
+
+#highscores
+
+score1 = 10
+score2 = 9
+score3 = 8
+score4 = 7 
+score5 = 6
+score6 = 5
+score7 = 4
+score8 = 3
+score9 = 2
+score10 = 1
+
+
+#printing highscores
+
+
+print("|---------------|")
+print("|   HIGHSCORE   |")
+print("|---------------|")
+print(name1 , str(score1))
+print(name2 , str(score2))
+print(name3 , str(score3))
+print(name4 , str(score4))
+print(name5 , str(score5))
+print(name6 , str(score6))
+print(name7 , str(score7))
+print(name8 , str(score8))
+print(name9 , str(score9))
+print(name10 , str(score10))
